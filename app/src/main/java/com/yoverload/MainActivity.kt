645615a4d3ae712c5fbf719569082e40697db29b
Tom <tom.j.egan88@gmail.com>
2019-01-03 21:05:30 +0000

    Initial commit

---
+++ app/src/main/java/com/yoverload/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.yoverload
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by tom.egan on 03-Jan-2019.
+ */
+class MainActivity() : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+    }
+}
